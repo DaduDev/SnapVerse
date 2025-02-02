c884325edeadbfe45bb07b7992e0a3ded761b53d
Shaik Dadapeer <69897636+DaduDev@users.noreply.github.com>
2025-02-02 17:06:15 +0530

    generated the models

---
+++ models/Video.ts
@@ -0,0 +1,40 @@
+import mongoose, { model, models, Schema }from "mongoose";
+
+export const VIDEO_DIM = {
+    width: 1080,
+    height: 1920,
+}
+
+export interface IVideo {
+    _id?: mongoose.Types.ObjectId,
+    title: string,
+    description: string,
+    url: string,
+    thumbnail: string,
+    controls?: boolean,
+    transformation?: {
+        width: number,
+        height: number,
+        quality?: number,
+    },
+    createdAt?: Date,
+    updatedAt?: Date,
+}
+
+const videoSchema = new Schema<IVideo>({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    url: {type: String, required: true},
+    thumbnail: {type: String, required: true},
+    controls: {type: Boolean, default: true},
+    transformation: {
+        width: {type: Number, default: VIDEO_DIM.width},
+        height: {type: Number, default: VIDEO_DIM.height},
+        quality: {type: Number, default: 80},
+    }
+}, {timestamps: true});
+
+const Video = models?.Video || model<IVideo>("Video", videoSchema)
+
+export default Video;
+
